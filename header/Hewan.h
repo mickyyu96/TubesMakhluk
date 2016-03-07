c7d3b06599b0f867b8ea551606290750ef406963
atikazzahra <atikazzahra@live.com>
2016-03-07 22:38:39 +0700

    Update Hewan.h

--- header/Hewan.h
+++ header/Hewan.h
@@ -8,13 +8,21 @@ class Hewan : public Makhluk {
 	public :
 		// virtual method
 		virtual void Move(int, int);
-		virtual void FindFood() = 0;
+		virtual int isVegetarian() = 0;
+        	//virtual void GetToFood() = 0;
 		
 		// getter
 		int getHungerLvl() { return hungerLvl; }
 		int getPower() { return power; }
 		int getDeltaT() { return deltaT;  }
 	
+		// setter
+        	void setHungerLvl(int x) { hungerLvl+=x; }
+    
+	     	Makhluk* FindFood();
+        	Makhluk* FindMakhluk(char _ID);
+        	int shouldRebounced(int dx, int dy);
+        
 	protected :
 		// ctor
 		Hewan(char, int);
