01c354ed33ecf432bb0eb7f2b99588d9b1295a3b
atikazzahra <atikazzahra@live.com>
2016-03-10 12:03:40 +0700

    Update Turtle.cpp

--- implementation/Turtle.cpp
+++ implementation/Turtle.cpp
@@ -1,4 +1,5 @@
 #include "../header/Turtle.h"
+#include "../header/LMakhluk.h"
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -14,6 +15,25 @@ Turtle::Turtle(const Point& P): Hewan(TURTLE_ID, TURTLE_MAXAGE){
 }
 
 void Turtle::Race(){
+    bool RabbitCome = false;
+    Point PRabbit;
+    
+    if (getisChallange()) {
+        PRabbit = Hewan::FindMakhluk('R')->getPosition();
+        if (PRabbit.getX() == 5 && PRabbit.getY()== 1) {
+            RabbitCome = true;
+        }
+        
+        Hewan::getToPoint(Point(5, 1));
+        while (!RabbitCome) {
+            PRabbit = Hewan::FindMakhluk('R')->getPosition();
+            if (PRabbit.getX() == 5 && PRabbit.getY()== 1) {
+                RabbitCome = true;
+                setIsChallange(0);
+            }
+        }
+        Hewan::getToPoint(Point(5, World::getWorldInstance()->getNKol()-2));
+    }
 }
 
 void Turtle::GetToFood(){
@@ -21,6 +41,26 @@ void Turtle::GetToFood(){
         Hewan::getToPoint(Hewan::FindFood()->getPosition());
         Hewan::FindFood()->Kill();
     }
+}
+
+int Turtle::isAnyTurtleRacing() {
+    int foundRacing = 0;
+    LMakhluk* _LMakhluk = new LMakhluk;
+    _LMakhluk->setFirst(World::getWorldInstance()->getObjects()->getFirst());
+    while (_LMakhluk->getFirst()!=World::getWorldInstance()->getObjects()->getLast() && !foundRacing) {
+        if (_LMakhluk->getFirst()->getInfo()->getID() == 'T' && _LMakhluk->getFirst()->getInfo()->isAlive()) {
+            if (static_cast<Turtle*>(_LMakhluk->getFirst()->getInfo())->getisChallange()) {
+                foundRacing = 1;
+            }
+        }
+        _LMakhluk->setFirst(_LMakhluk->getFirst()->getNext());
+    }
+    if (_LMakhluk->getFirst()->getInfo()->getID() == 'R' && _LMakhluk->getFirst()->getInfo()->isAlive() && !foundRacing) {
+        if (static_cast<Turtle*>(_LMakhluk->getFirst()->getInfo())->getisChallange()) {
+            foundRacing = 1;
+        }
+    }
+    return foundRacing;
 }
 
 // main action
