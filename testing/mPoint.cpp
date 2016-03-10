8358b7146950153d14f5cae915a9964d15ee9618
Ngiong <ngiooong@yahoo.com>
2016-03-10 08:33:26 +0700

    Benerin testing, khusus mWorld skrg buat testing KeypressHandler.. xD

--- testing/mPoint.cpp
+++ testing/mPoint.cpp
@@ -5,17 +5,25 @@ using namespace std;
 
 int main()
 {
+	cout << "Testing default ctor" << endl;
 	Point P;
 	cout << P.getX() << "," << P.getY() << endl;
 	
+	cout << "Testing ctor dengan koordinat (42,25)" << endl;
 	Point Q(42,25);
 	cout << Q.getX() << "," << Q.getY() << endl;
 	
+	cout << "Testing cctor dengan mengopy Point yang terakhir dibuat (42,25)" << endl;
 	Point R = Q;
 	cout << R.getX() << "," << R.getY() << endl;
 
+	cout << "Testing prosedur increment untuk translasi Point (42,25) dengan vektor (1,-1)" << endl;
 	R.increment(1, -1);
 	cout << R.getX() << "," << R.getY() << endl;
+	
+	cout << "Testing fungsi getDistance titik (42,25) dengan koordinat (0, 0)" << endl;
+	Point S = Point(0,0);
+	cout << Point::getDistance(Q, S)<< endl;
 	
 	return 0;
 }
--- testing/mWorld.cpp
+++ testing/mWorld.cpp
@@ -1,15 +1,20 @@
 // mWorld.cpp
 #include "../header/World.h"
-#include "../header/Makhluk.h"
+#include "../header/LMakhluk.h"
+#include "../header/KeypressHandler.h"
 #include <algorithm>
 #include <thread>
 using namespace std;
 
 int main()
 {
-	//World::getWorldInstance()->PrintMap();
-	thread t1(Makhluk::MakeAlive, World::getWorldInstance()->getFirstMakhluk());
+	thread t1(Makhluk::MakeAlive, World::getWorldInstance()->getObjects()->getFirst()->getInfo());
+	thread t2(World::Show, 500);
+	thread t3(KeypressHandler::HandleKeypress);
 	t1.join();
+	t2.join();
+	t3.join();
+
 	system("PAUSE");
 	return 0;
 }
