513d9fd3acf2ace56e3eff9e71fcd2a7d5518ccb
Micky Yu <mickyyu96@gmail.com>
2016-03-11 05:25:11 +0700

    setIsChallange + makefile

--- header/Turtle.h
+++ header/Turtle.h
@@ -18,14 +18,14 @@ class Turtle : public Hewan {
      *	\return Objek bertipe Turtle
      */
     Turtle(const Point&);
-    
+
     /** \brief Validasi apakah Turtle merupakan Vegetarian
      *  \details Validasi akan selalu mengembalikan integer bernilai 1 sebagai
-     *   validasi bahwa Turtle merupakan binatang vegetarian 
+     *   validasi bahwa Turtle merupakan binatang vegetarian
      *	\return integer bernilai 1
      */
     int isVegetarian() { return 1; }
-    
+
     /** \brief Getter isChallange milik Turtle
      *  \details Fungsi getter yang akan mengambilkan nilai isChallange dari Turtle.
      *  Bernilai 0 jika tidak ada yang mengajak untuk race dan bernilai 1 jika ada Rabbit
@@ -33,42 +33,44 @@ class Turtle : public Hewan {
      *	\return Integer
      */
     int getisChallange() { return isChallange; }
-    
+
+    void setIsChallange(int a) { isChallange = a; }
+
     /** \brief Getter DeltaT Turtle
      *  \details Fungsi getter yang akan mengambilkan nilai deltaT dari Turtle
      *  yang merupakan member dari kelas hewan yang diinisialisasi saat ctor
      *	\return Integer DeltaT Turtle
      */
     int getDeltaT() { return Hewan::getDeltaT(); }
-    
+
     /** \brief Procedure aksi untuk Turtle melakukan Race dengan objek Rabbit
      *  \details Procedure aksi Race ini akan melakukan validasi apakah ada objek Rabbit yang
      *  mengajak race. Jika ada maka Turtle pergi ke point start, dan melakukan race bersama Rabbit ke finish point
      *	\return void
      */
     void Race();
-    
+
     /** \brief Procedure aksi untuk Rabbit mendapatkan makanan
      *  \details Procedure aksi GetToFood membuat Rabbit pergi ke point dimana makanannya
      *  berasa dan memakannya
      *	\return void
      */
     void GetToFood();
-    
+
     /** \brief Validasi apakah ada objek Turtle yang sedang melakukan Race bertipe static function
      *  \details Validasi akan melakukan pencarian Makhluk Turtle pada listMakhluk yang
      *  nilai isChallange adalah 1
      *	\return integer
      */
     static int isAnyTurtleRacing();
-    
+
     /** \brief Procedure aksi utama yang mengatur hidup Turtle sampai mati
      *  \details Procedure aksi utama akan memilih aktivitas kehidupan yang akan dilakukan
      *  oleh Turtle selama hidupnya
      *	\return void
      */
     void Live();
-    
+
     private :
     static const char TURTLE_ID = 'T'; /**< ID Turtle*/
     static const int TURTLE_MAXAGE = 15; /**< Umur maksimal Turtle*/
