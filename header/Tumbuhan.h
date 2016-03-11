9aec05569f88494b7f4d1891b0c5898cb098515e
elvinvina <vina9196@gmail.com>
2016-03-11 20:43:08 +0700

    Doxygen & menghilangkan ctor tanpa parameter

--- header/Tumbuhan.h
+++ header/Tumbuhan.h
@@ -4,17 +4,31 @@
 
 #include "Makhluk.h"
 
+/** \class Tumbuhan
+*	\brief Kelas Turunan dari Makhluk berupa Tumbuhan
+*	\details Kelas Tumbuhan merupakan kelas yang berfungsi untuk mengatur behaviour dan kehidupan dari objek Tumbuhan
+*	\author Elvina Riama K. Situmorang
+*	\date Maret 2016
+*/
+
 class Tumbuhan : public Makhluk {
 	public :
-		//ctor
-		Tumbuhan();
+		/** \brief Constructor dari objek bertipe Tumbuhan
+	     	*  \details Constructor Tumbuhan yang akan membentuk objek bertipe Tumbuhan
+	     	*   dengan masukan variable reference Point sebagai posisi awal objek
+	     	*	\return Objek bertipe Tumbuhan
+	     	*/
 		Tumbuhan(const Point& P);
 
-		//main thread
+		/** \brief Procedure aksi utama yang mengatur hidup Tumbuhan sampai mati
+		*  \details Procedure aksi utama akan memilih aktivitas kehidupan yang akan dilakukan
+		*  oleh Tumbuhan selama hidupnya
+		*	\return void
+		*/
 		void Live();
 	private:
-		static const char TUMBUHAN_ID = 'G';
-		static const int TUMBUHAN_MAXAGE = 100;
+		static const char TUMBUHAN_ID = 'G';	/**< ID Tumbuhan*/
+		static const int TUMBUHAN_MAXAGE = 100;	/**< Umur maksimal Tumbuhan*/
 };
 
 #endif
