d4c358ea90642da83e79e002512f306358b408f6
atikazzahra <atikazzahra@live.com>
2016-03-18 22:55:17 +0700

    merapihkan oxygen

--- header/Turtle.h
+++ header/Turtle.h
@@ -3,34 +3,34 @@
 #define Turtle_h
 #include "Hewan.h"
 
-/** \class Turtle
- *  \brief Kelas Turunan dari Makhluk berupa Rabbit
- *  \details Kelas Rabbit merupakan kelas yang berfungsi untuk mengatur behaviour dan kehidupan dari objek Rabbit
- *	\author Atika Azzahra Akbar
- *	\date Maret 2016
+/** \class 	Turtle
+ *  \brief 	Kelas Turunan dari Makhluk berupa Rabbit
+ *  \details 	Kelas Rabbit merupakan kelas yang berfungsi untuk merepresentasikan objek Rabbit
+ *	\author 	Atika Azzahra Akbar
+ *	\date 		Maret 2016
  */
 
 class Turtle : public Hewan {
     public :
-    /** \brief Constructor dari objek bertipe Turle
-     *  \details Constructor Turtle yang akan membentuk objek bertipe Turtle
-     *   dengan masukan variable reference Point sebagai posisi awal objek
-     *	\return Objek bertipe Turtle
+    /** \brief 		Constructor
+     *  \details 	Constructor Turtle yang akan membentuk objek bertipe Turtle
+     *   		dengan masukan variable reference Point sebagai posisi awal objek
+     *  \param		konstanta reference Point untuk posisi awal objek
      */
     Turtle(const Point&);
 
-    /** \brief Validasi apakah Turtle merupakan Vegetarian
-     *  \details Validasi akan selalu mengembalikan integer bernilai 1 sebagai
-     *   validasi bahwa Turtle merupakan binatang vegetarian
-     *	\return integer bernilai 1
+    /** \brief 		Validasi apakah Turtle merupakan Vegetarian
+     *  \details 	Validasi akan selalu mengembalikan integer bernilai 1 sebagai
+     *  		validasi bahwa Turtle merupakan binatang vegetarian
+     *	\return 	TRUE
      */
     int isVegetarian() { return 1; }
 
-    /** \brief Getter isChallange milik Turtle
-     *  \details Fungsi getter yang akan mengambilkan nilai isChallange dari Turtle.
-     *  Bernilai 0 jika tidak ada yang mengajak untuk race dan bernilai 1 jika ada Rabbit
-     *  yang mengajak
-     *	\return Integer
+    /** \brief 		Getter isChallange milik Turtle
+     *  \details 	Fungsi getter yang akan mengambilkan nilai isChallange dari Turtle.
+     *  		Bernilai 0 jika tidak ada yang mengajak untuk race dan bernilai 1
+     *  		jika ada Rabbit yang mengajak
+     *	\return 	TRUE bila ada Rabbit yang mengajak dan FALSE bila tidak ada
      */
     int getisChallange() { return isChallange; }
 
@@ -43,31 +43,32 @@ class Turtle : public Hewan {
      */
     int getDeltaT() { return Hewan::getDeltaT(); }
 
-    /** \brief Procedure aksi untuk Turtle melakukan Race dengan objek Rabbit
-     *  \details Procedure aksi Race ini akan melakukan validasi apakah ada objek Rabbit yang
-     *  mengajak race. Jika ada maka Turtle pergi ke point start, dan melakukan race bersama Rabbit ke finish point
-     *	\return void
+    /** \brief 		Procedure aksi untuk Turtle melakukan Race dengan objek Rabbit
+     *  \details 	Procedure aksi Race ini akan melakukan validasi apakah ada objek Rabbit yang
+     *  		mengajak race. Jika ada maka Turtle pergi ke point start, dan melakukan race 
+     * 			bersama Rabbit ke finish point
+     *	\return 	void
      */
     void Race();
 
-    /** \brief Procedure aksi untuk Rabbit mendapatkan makanan
-     *  \details Procedure aksi GetToFood membuat Rabbit pergi ke point dimana makanannya
-     *  berasa dan memakannya
-     *	\return void
+    /** \brief 		Procedure aksi untuk Rabbit mendapatkan makanan
+     *  \details 	Procedure aksi GetToFood membuat Rabbit pergi ke point dimana makanannya
+     *  		berasa dan memakannya
+     *	\return 	void
      */
     void GetToFood();
 
-    /** \brief Validasi apakah ada objek Turtle yang sedang melakukan Race bertipe static function
-     *  \details Validasi akan melakukan pencarian Makhluk Turtle pada listMakhluk yang
-     *  nilai isChallange adalah 1
-     *	\return integer
+    /** \brief 		Validasi apakah ada objek Turtle yang sedang melakukan Race bertipe static function
+     *  \details 	Validasi akan melakukan pencarian Makhluk Turtle pada listMakhluk yang
+     *  		nilai isChallange adalah 1
+     *	\return 	integer
      */
     static int isAnyTurtleRacing();
 
-    /** \brief Procedure aksi utama yang mengatur hidup Turtle sampai mati
-     *  \details Procedure aksi utama akan memilih aktivitas kehidupan yang akan dilakukan
-     *  oleh Turtle selama hidupnya
-     *	\return void
+    /** \brief 		Procedure aksi utama yang mengatur hidup Turtle sampai mati
+     *  \details 	Procedure aksi utama akan memilih aktivitas kehidupan yang akan dilakukan
+     *  		oleh Turtle selama hidupnya
+     *	\return 	void
      */
     void Live();
 
