0fe185d7702b9f79dd5b05193b19d5ab5f839125
Micky Yu <mickyyu96@gmail.com>
2016-03-21 05:55:16 +0700

    unit test tumbuhan

---
+++ v3/testing/mTumbuhan.cpp
@@ -0,0 +1,42 @@
+// mTumbuhan.cpp
+
+#include "../header/World.h"
+#include "../header/LMakhluk.h"
+#include "../header/WorldBuilder.h"
+#include "../header/Screen.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <thread>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "-----------------------Tumbuhan------------------------" << endl;
+    cout << "-------------------------------------------------------" << endl;
+    cout << "Tumbuhan merupakan makhluk yang memiliki behavior diam." << endl;
+    cout << "-------------------------------------------------------" << endl;
+    std::chrono::milliseconds timespan(3000);
+    std::this_thread::sleep_for(timespan);
+    World::getWorldInstance()->setNBrs(12); World::getWorldInstance()->setNKol(12);
+    string input = "T";
+    WorldBuilder::getBuilderInstance()->setStrMakhluk(input);
+	WorldBuilder::getBuilderInstance()->buildWorldObjects();
+	thread t[55];
+	int thread_count = 0;
+
+	t[thread_count++] = thread(Screen::ShowWorld, 500);
+
+	for(int i=0; i<thread_count; i++)
+	{
+		t[i].join();
+	}
+
+	cout << "The Program Ends Here.\n" << endl;
+	system("PAUSE");
+	delete World::getWorldInstance();
+	return 0;
+}
+
