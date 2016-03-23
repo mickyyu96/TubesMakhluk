914d635938de6d87feb483d8e748ad9199ac1156
Ngiong <ngiooong@yahoo.com>
2016-03-23 20:49:49 +0700

    Masih ada typo

--- 13514011_epic/src/H-1/implementation/Turtle.cpp
+++ 13514011_epic/src/H-1/implementation/Turtle.cpp
@@ -39,7 +39,7 @@ void Turtle::Race()
             }
             else
             {
-                RabbitCom = true;
+                RabbitCome = true;
                 setIsChallange(0);
             }
         }
