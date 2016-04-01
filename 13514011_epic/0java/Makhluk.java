cb057164b6af89f2042a2c7543e6b25e1da6bc24
atikazzahra <atikazzahra@live.com>
2016-04-01 11:10:52 +0700

    Update Makhluk.java

--- 13514011_epic/0java/Makhluk.java
+++ 13514011_epic/0java/Makhluk.java
@@ -1,3 +1,9 @@
+/** Kelas Makhluk merupakan kelas abstrak yang memiliki member-member dasar yang akan diturunkan ke kelas lain.
+ *	@author     Atika Azzahra Akbar
+ *	@date       April 2016
+ *	@version    1.0
+ */
+ 
 public class Makhluk {
 	protected final char ID;     
 	protected final int maxAge;   
@@ -6,6 +12,8 @@ public class Makhluk {
 	protected Point pos;
 	
 	/** Menciptakan sebuah makhluk
+	 *  @param	char ID Makhluk
+	 *  @param 	int age maksimal Makhluk
 	 */
 	protected Makhluk(char C, int A){
 		ID = C;
@@ -13,34 +21,33 @@ public class Makhluk {
 	}
 	
 	/** Mengambil sebuah karakter ID makhluk
-	*	@return		Karakter ID makhluk
+	*   @return	Karakter ID makhluk
 	*/
 	public final char getID() { return ID; }
 	
 	/** Mengambil usia makhluk sekarang
-	*	@return		Sebuah bilangan bulat yang merupakan usia makhluk sekarang
+	*   @return	Sebuah bilangan bulat yang merupakan usia makhluk sekarang
 	*/
 	public final int getAge() { return age; }
 	
 	/** Mengambil usia maksimum makhluk
-	*	@return		Sebuah bilangan bulat yang merupakan usia maksimum makhluk
+	*   @return	Sebuah bilangan bulat yang merupakan usia maksimum makhluk
 	*/
 	public final int getMaxAge() { return maxAge; }
 	
 	/** Mengambil posisi makhluk sekarang
-	*	@return		Sebuah tipe bentukan Point yang merupakan posisi makhluk
+	*   @return	Sebuah tipe bentukan Point yang merupakan posisi makhluk
 	*/
 	public final Point getPosition() { return pos; }
 	
 	/** Melakukan assignment terhadap posisi makhluk dengan posisi baru
-	*	@param		P const Point& , sebuah posisi baru makhluk
-	*	@return		void
+	*   @param	P const Point& , sebuah posisi baru makhluk
+	*   @return	void
 	*/
 	public void setPosition(final Point P){ pos = P; }
 	
-	/** \brief      Fungsi validasi
-	*	\details    Fungsi untuk mengecek apakah 2 makhluk berada dalam point yang sama
-	*	\return     Bilangan bulat 0 atau 1
+	/** Fungsi validasi untuk mengecek apakah 2 makhluk berada dalam point yang sama
+	*   @return     Bilangan bulat 0 atau 1
 	*/
 	public int isMakhlkukInTheSamePoint(){
 		int found = 0;
@@ -60,8 +67,9 @@ public class Makhluk {
 	    return found;
 	}
 	
-	/** Fungsi validasi untuk mengecek apakah 2 makhluk berada dalam point yang sama
-	*	@return     Bilangan bulat 0 atau 1
+	/**  Prosedur yang mengembalikan makhluk yang berada pada point yang sama dengan objek.
+        *    Objek dipastikan bersama dengan makhluk lain pada suatu point.
+	*    @return     Pointer to makhluk
 	*/
 	public Makhluk MakhlukInTheSamePoint(){
 		Makhluk _Makhluk;
@@ -84,18 +92,17 @@ public class Makhluk {
 	}
 	
 	/** Mengambil status ke"hidup"an makhluk
-	*	@return		True apabila makhluk masih hidup
+	*   @return	True apabila makhluk masih hidup
 	*/
 	public int isAlive() { if ( status == 1 ) return 1; else return 0; }
 	
-	/** Prosedur yang digunakan untuk membunuh makhluk dari alam semesta, yaitu apabila
-	*				dimangsa atau terlalu tua
-	*	@return		void
+	/** Prosedur yang digunakan untuk membunuh makhluk dari alam semesta, yaitu apabila dimangsa atau terlalu tua
+	*   @return	void
 	*/
 	public void Kill() { status =  0; }
 	
 	/** Prosedur untuk menambah usia objek
-	*	@return     void
+	*   @return     void
 	*/
 	public void AgeIncrement() { age++; }
 }
