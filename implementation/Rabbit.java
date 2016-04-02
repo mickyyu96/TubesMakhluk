73d16c8beb39e5eb0aba6e34a89214e076362f02
elvinvina <vina9196@gmail.com>
2016-04-02 23:51:29 +0700

    Added files via upload
    upload seadanya dulu

---
+++ implementation/Rabbit.java
@@ -0,0 +1,16 @@
+public class Rabbit extends Hewan
+{
+	private static final char RABBIT_ID = 'R';
+	private static final int RABBIT_MAXAGE = 100;
+	private static final int RABBIT_BASEPOWER = 3;
+
+	Rabbit (final Point P)
+	{
+		super(RABBIT_ID, RABBIT_MAXAGE);
+
+		age = 0;
+		status = 1;
+		power = RABBIT_BASEPOWER;
+		pos = P;
+	}
+}
