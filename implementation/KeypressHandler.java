c8c72e39a6d0806385f3e6988cdec399ae800dd1
Ngiong <ngiooong@yahoo.com>
2016-04-04 12:39:46 +0700

    Benerin LMakhluk... Program sudah bisa berjalan, kurang Keypress aja :)

--- implementation/KeypressHandler.java
+++ implementation/KeypressHandler.java
@@ -15,9 +15,60 @@ class KeypressHandler implements KeyListener {
     private char lastKeypress;
     
     private KeypressHandler() {}
+
+    public char getLastKeypress() { return lastKeypress; }
     
     public static KeypressHandler getHandlerInstance() {
         return handlerInstance;
+    }
+
+    public void getKeypress() throws Exception {
+        //lastKeypress = (char) System.in.read(); <<masih gak bisa ternyata :( >>
+    }
+
+    public void doAction() throws Exception {
+        char c = getHandlerInstance().getLastKeypress();
+        switch (c)
+        {
+            case 'q':
+                World.getWorldInstance().endWorld();
+                break;
+            case 'w':
+                lastKeypress = '-';
+                do
+                {
+                    while(System.in.available()!=0)
+                    {
+                        KeypressHandler.getHandlerInstance().getKeypress();
+                        if(lastKeypress == '.')
+                        {
+                            MakhlukLive.getInstance().MakhlukMove();
+                            Screen.getScreenInstance().PrintWorldMap();
+                            MakhlukLive.getInstance().MakhlukEat();
+                        }
+                        else if(lastKeypress == 'c')
+                        {
+                            SnapshotCapturer.getCapturerInstance().captureSnapshot();
+                        }
+                    }
+                }while(lastKeypress != 'w');
+                break;
+            case 'c':
+                SnapshotCapturer.getCapturerInstance().captureSnapshot();
+                break;
+            case '.':
+                try
+                {
+                    throw new ExceptionObject(2);
+                }
+                catch(ExceptionObject E)
+                {
+                    E.DisplayErrorMessage();
+                }
+                break;
+            default :
+                WorldBuilder.getBuilderInstance().addAnObject(c);
+        }
     }
     
     public void keyTyped(KeyEvent e) {}
