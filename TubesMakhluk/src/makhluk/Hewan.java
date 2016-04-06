cf3133fc50012fe450101f322df89a1f2e466083
atikazzahra <atikazzahraa@gmail.com>
2016-04-06 13:17:18 +0700

    checkstyle

--- TubesMakhluk/src/makhluk/Hewan.java
+++ TubesMakhluk/src/makhluk/Hewan.java
@@ -9,40 +9,43 @@ import world.*;
  *	@version	1.0
  */
 public class Hewan extends Makhluk {
-	/** Atribut hungerLvl, power, deltaT hewan.
-	 */
-    protected int hungerLvl, power, deltaT;
-    /** Konstruktor Hewan, yang akan dilakukan passing parameter kepada kelas Makhluk.
-     *  @param char ID dari Hewan
-     *  @param int Maximum umur seekor Hewan
+    /** Atribut hungerLvl, power, deltaT hewan.
      */
-    protected Hewan (char _ID, int _maxAge) {
-        super(_ID,_maxAge);
+    protected int hungerLvl, power, deltaT;
+    /** Konstruktor Hewan, yang akan dilakukan passing parameter
+     *  kepada kelas Makhluk.
+     *  @param id dari Hewan
+     *  @param maxAge umur seekor Hewan
+     */
+    protected Hewan(final char id, final int maxAge) {
+        super(id, maxAge);
     }
-	/** Mengatur pergerakan makhluk.
-	 *	@param 	int dx, besar perpindahan makhluk yaitu dx
-	 *	@param 	int dy, besar perpindahan makhluk yaitu dy
-	 *	@return	void
-	 */
-    public void Move (int dx, int dy) {
+    /** Mengatur pergerakan makhluk.
+     *  @param dx besar perpindahan makhluk yaitu dx
+     *  @param dy besar perpindahan makhluk yaitu dy
+     */
+    public final void move(final int dx, final int dy) {
          pos.increment(dx, dy);
     }
     /** Mengembalikan power yang dimiliki seekor Hewan.
      *  @return int power seekor Hewan
      */
-    public int getPower() {
+    public final int getPower() {
         return power;
     }
     /** Mengembalikan apakah hewan tersebut akan bergerak menabrak.
-     *	@param 	int dx, besar perpindahan makhluk yaitu dx
-     * 	@param 	int dy, besar perpindahan makhluk yaitu dy
-	 *	@return	boolean akan bernilai true jika hewan tersebut harus bergerak ke arah lainnya
+     *  @param dx besar perpindahan makhluk yaitu dx
+     *  @param dy besar perpindahan makhluk yaitu dy
+     *  @return boolean akan bernilai true jika hewan tersebut harus
+     *  bergerak ke arah lainnya
      */
-    public boolean shouldRebounced(int dx, int dy) {
-        if (dx+pos.getX() <= 0 || dx+pos.getX() >= World.getWorldInstance().getNBrs()-1) {
+    public final boolean shouldRebounced(final int dx, final int dy) {
+        if (dx + pos.getX() <= 0 || dx + pos.getX()
+             >= World.getWorldInstance().getNBrs() - 1) {
             return true;
         }
-        if (dy+pos.getY() <= 0 || dy+pos.getY() >= World.getWorldInstance().getNKol()-1) {
+        if (dy + pos.getY() <= 0 || dy + pos.getY()
+            >= World.getWorldInstance().getNKol() - 1) {
             return true;
         }
         return false;
