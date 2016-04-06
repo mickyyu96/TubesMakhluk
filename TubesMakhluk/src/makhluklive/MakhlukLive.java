7a698856a9da271459a454db8ee9ad4a05a52757
atikazzahra <atikazzahraa@gmail.com>
2016-04-06 16:52:03 +0700

    checkstyle

--- TubesMakhluk/src/makhluklive/MakhlukLive.java
+++ TubesMakhluk/src/makhluklive/MakhlukLive.java
@@ -1,95 +1,102 @@
-//MakhlukLive.java
-
 package makhluklive;
 
-import makhluk.*;
-import randomgenerator.*;
-import world.*;
+import makhluk.Makhluk;
+import makhluk.Hewan;
+import randomgenerator.RandomGenerator;
+import world.World;
+import world.LMakhluk;
 
 /** Kelas MakhlukLive merupakan kelas yang menangani kehidupan dari setiap makhluk
  *	Kehidupan yaitu pergerakan, pencarian makan, dan umur hidup 
  *	@author     Elvina R. K. Situmorang / 13514045
  *	@version    1.0
  */
-public class MakhlukLive
-{
-	private static MakhlukLive _instance = new MakhlukLive();
-	private int dx_sign = 1;
-	private int dy_sign = 1;
-	/** Konstruktor menciptakan MakhlukLive
-	 */
-	private MakhlukLive() {
+public final class MakhlukLive {
+    /** Atribut singleton.
+     */
+    private static MakhlukLive instance = new MakhlukLive();
+    /** Atribut dx.
+     */
+    private int dxsign = 1;
+    /** Atribut dy.
+     */
+    private int dysign = 1;
+    /** Konstruktor menciptakan MakhlukLive.
+     */
+    private MakhlukLive() {
     }
-	/** Mengembalikan pointer dari objek kelas singleton pada kelas MakhlukLive
-	 *	@return	Pointer yang menunjuk ke singleton instance pada kelas MakhlukLive
-	 */
-	public static MakhlukLive getInstance() {
-        return _instance;
+    /** Mengembalikan pointer dari objek kelas singleton pada kelas MakhlukLive.
+     *  @return Pointer yang menunjuk ke singleton instance pada
+     *  kelas MakhlukLive
+     */
+    public static MakhlukLive getInstance() {
+        return instance;
     }
-	/** Menangani pergerakan dari makhluk
-	 *  @return  void
-	 */
-	public void MakhlukMove() {
-		int dx, dy;
-		LMakhluk _LMakhluk = World.getWorldInstance().getObjects();
+    /** Menangani pergerakan dari makhluk.
+     */
+    public void makhlukMove() {
+        int dx, dy;
+        LMakhluk lMakhluk = World.getWorldInstance().getObjects();
 
-		for (int i = 0; i<_LMakhluk.getSize(); i++) {
-	        if (_LMakhluk.getInfo(i).isAlive()==1 && _LMakhluk.getInfo(i).getID()!='G') {
-	            Hewan _Hewan = (Hewan)_LMakhluk.getInfo(i);
+        for (int i = 0; i < lMakhluk.getSize(); i++) {
+                if (lMakhluk.getInfo(i).isAlive() == 1
+                     && lMakhluk.getInfo(i).getID() != 'G') {
+                Hewan hewan = (Hewan) lMakhluk.getInfo(i);
 
-	            dx = RandomGenerator.getInstance().getNextInt(2)*dx_sign;
-	            dy = RandomGenerator.getInstance().getNextInt(2)*dy_sign;
+                dx = RandomGenerator.getInstance().getNextInt(2) * dxsign;
+                dy = RandomGenerator.getInstance().getNextInt(2) * dysign;
 
-	            if (_Hewan.shouldRebounced(dx, 0)) {
-	                dx *= -1;
-	                dx_sign *= -1;
-	            }
-	            if (_Hewan.shouldRebounced(0, dy)) {
-	                dy *= -1;
-	                dy_sign *= -1;
-	            }
+               if (hewan.shouldRebounced(dx, 0)) {
+                   dx *= -1;
+                   dxsign *= -1;
+               }
+               if (hewan.shouldRebounced(0, dy)) {
+                    dy *= -1;
+                    dysign *= -1;
+               }
 
-	            _Hewan.Move(dx, dy);
-	            _Hewan.ageIncrement();
-	            if(_Hewan.getAge()==_Hewan.getMaxAge()) {
-	                _Hewan.kill();
-	            }
-	        }
-	        else {
-	            _LMakhluk.getInfo(i).ageIncrement();
-	            if(_LMakhluk.getInfo(i).getAge()==_LMakhluk.getInfo(i).getMaxAge()) {
-	                _LMakhluk.getInfo(i).kill();
-	            }
-	        }
-	    }
-	}
-	/** Menangani pergerakan dari makhluk untuk mencari makan
-	 *  @return  void
-	 */
-	public void MakhlukEat(){
-		World worldInstance = World.getWorldInstance();
+                hewan.move(dx, dy);
+                hewan.ageIncrement();
+                if (hewan.getAge() == hewan.getMaxAge()) {
+                    hewan.kill();
+                }
+         } else {
+                lMakhluk.getInfo(i).ageIncrement();
+                if (lMakhluk.getInfo(i).getAge()
+                       == lMakhluk.getInfo(i).getMaxAge()) {
+                   lMakhluk.getInfo(i).kill();
+                }
+            }
+        }
+    }
+    /** Menangani pergerakan dari makhluk untuk mencari makan.
+     */
+    public void makhlukEat() {
+        World worldInstance = World.getWorldInstance();
+        LMakhluk lMakhluk = worldInstance.getObjects();
 
-		LMakhluk _LMakhluk = worldInstance.getObjects();
-
-		for (int i = 0; i<_LMakhluk.getSize(); i++) {
-	        if (_LMakhluk.getInfo(i).isAlive() == 1 && _LMakhluk.getInfo(i).getID()!='G') {
-	            if (_LMakhluk.getInfo(i).isMakhlukInTheSamePoint() == 1){
-	                Makhluk M1 = _LMakhluk.getInfo(i);
-	                Makhluk M2 = _LMakhluk.getInfo(i).makhlukInTheSamePoint();
-	                if (((M1).getID()=='R')||((M1).getID()=='T')||((M1).getID()=='S')) {
-	                    if ((M2).getID()=='G'){
-	                        M2.kill();
-	                    }
-	                }
-	                else {
-	                    if (M2.getID()!='G') {
-	                        if (((Hewan)M2).getPower()<((Hewan) M1).getPower()) {
-	                            M2.kill();
-	                        }
-	                    }
-	                }
-	            }
-	        }
-    	}		
-	}
+        for (int i = 0; i < lMakhluk.getSize(); i++) {
+            if (lMakhluk.getInfo(i).isAlive() == 1
+                    && lMakhluk.getInfo(i).getID() != 'G') {
+                if (lMakhluk.getInfo(i).isMakhlukInTheSamePoint() == 1) {
+                    Makhluk m1 = lMakhluk.getInfo(i);
+                    Makhluk m2 =
+                            lMakhluk.getInfo(i).makhlukInTheSamePoint();
+                    if (((m1).getID() == 'R') || ((m1).getID() == 'T')
+                            || ((m1).getID() == 'S')) {
+                        if ((m2).getID() == 'G') {
+                            m2.kill();
+                        }
+                    } else {
+                        if (m2.getID() != 'G') {
+                            if (((Hewan) m2).getPower()
+                                < ((Hewan) m1).getPower()) {
+                                m2.kill();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
