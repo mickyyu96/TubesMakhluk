82f085030896969fe60197510096bbcb86dc73fe
elvinvina <vina9196@gmail.com>
2016-04-07 07:11:20 +0700

    benerin error

--- TubesMakhluk/src/exception/ExceptionObjectTest.java
+++ TubesMakhluk/src/exception/ExceptionObjectTest.java
@@ -16,14 +16,14 @@ public class ExceptionObjectTest {
         public void test_DisplayErrorMsg() {
             System.out.println("Tes apakah Pesan Error benar atau tidak");
             ExceptionObject E1 = new ExceptionObject(0);
-            assertEquals(">> ERROR! Dimensi ukuran dunia harus > 6!",E1.GetMessage());
+            assertEquals(">> ERROR! Dimensi ukuran dunia harus > 6!",E1.getMessage());
             ExceptionObject E2 = new ExceptionObject(1);
-            assertEquals(">> ERROR! Ada ID yang tidak dikenali!",E2.GetMessage());
+            assertEquals(">> ERROR! Ada ID yang tidak dikenali!",E2.getMessage());
             ExceptionObject E3 = new ExceptionObject(2);
             assertEquals(">> ERROR! Pause World dulu sebelum melakukan Single-step Execution!",E3.GetMessage());
             ExceptionObject E4 = new ExceptionObject(3);
-            assertEquals(">> ERROR! Keypress tidak dikenali!",E4.GetMessage());
+            assertEquals(">> ERROR! Keypress tidak dikenali!",E4.getMessage());
             ExceptionObject E5 = new ExceptionObject(4);
-            assertEquals(">> ERROR! Masukan harus berupa bilangan bulat!", E5.GetMessage());
+            assertEquals(">> ERROR! Masukan harus berupa bilangan bulat!", E5.getMessage());
         }
 }
--- TubesMakhluk/src/world/LMakhlukTest.java
+++ TubesMakhluk/src/world/LMakhlukTest.java
@@ -12,9 +12,9 @@ public class LMakhlukTest {
     @Test
     public void test_LMakhluk() {
         LMakhluk _LMakhluk = new LMakhluk();
-        _LMakhluk.Add(new Wolf(RandomGenerator.getInstance().getNextPoint(5, 5)));
-        _LMakhluk.Add(new Sheep(RandomGenerator.getInstance().getNextPoint(5, 5)));
-        _LMakhluk.Add(new PolarBear(RandomGenerator.getInstance().getNextPoint(5, 5)));
+        _LMakhluk.addd(new Wolf(RandomGenerator.getInstance().getNextPoint(5, 5)));
+        _LMakhluk.add(new Sheep(RandomGenerator.getInstance().getNextPoint(5, 5)));
+        _LMakhluk.add(new PolarBear(RandomGenerator.getInstance().getNextPoint(5, 5)));
 
         char c;
         c = _LMakhluk.getInfo(0).getID();
